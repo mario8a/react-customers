f2fd1c00a9d90bc0652b533b76450b67835c5c83
Mario Ochoa <mario8ato@hotmail.com>
2020-05-28 18:03:22 -0500

    Formulario de edicion

--- src/components/CustomerEdit.jsx
+++ src/components/CustomerEdit.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {reduxForm, Field} from 'redux-form';
 
 const CustomerEdit = ({name, dni, age}) => {
     return (
         <div>
             <h2>Ediion del cliente</h2>
-            <h3> Name: {name} / DNI: {dni} /  Age: {age} </h3>
+            <form action="">
+                <div>
+                    <label htmlFor="name">Nombre</label>
+                    <Field name="name" component="input" type="text" ></Field>
+                </div>
+                <div>
+                    <label htmlFor="dni">Dni</label>
+                    <Field name="dni" component="input" type="text" ></Field>
+                </div>
+                <div>
+                    <label htmlFor="age">Edad</label>
+                    <Field name="age" component="input" type="number" ></Field>
+                </div>
+            </form>
         </div>
     );
 };
@@ -16,4 +30,4 @@ CustomerEdit.propTypes = {
     age: PropTypes.number,
 };
 
-export default CustomerEdit;
+export default reduxForm( {form: 'CustomerEdit'})(CustomerEdit);
