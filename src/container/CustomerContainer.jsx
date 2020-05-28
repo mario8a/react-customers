429f3324c6430f91f4c0623d50400f5780595375
Mario Ochoa <mario8ato@hotmail.com>
2020-05-27 22:02:56 -0500

    Type en ejecucion

--- src/container/CustomerContainer.jsx
+++ src/container/CustomerContainer.jsx
@@ -3,15 +3,28 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types'
 import AppFrame from '../components/AppFrame';
 import { getCustomerByDni } from '../selectors/customers';
+import { Route } from 'react-router-dom';
+import CustomerEdit from '../components/CustomerEdit';
+import CustomerData from '../components/CustomerData';
 
 class CustomerContainer extends Component {
 
+    renderBody = () => (
+        <Route path="/customers/:dni/edit" children= {
+            ({match}) => {
+                const CustomerControl = match ? CustomerEdit : CustomerData;
+                return <CustomerControl {...this.props.customer} />
+            }
+        } />
+    )   
+
+    //<p>Datos del cliente "{this.props.customer.name}" </p> 
 
     render() {
         return (
             <div>
                 <AppFrame header={`Cliente ${this.props.dni}`}
-                    body={ <p>Datos del cliente "{this.props.customer.name}" </p> }>
+                    body={this.renderBody()}>
                 </AppFrame>
             </div>
         )
